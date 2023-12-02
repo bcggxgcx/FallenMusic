9113e180a14fc8b055cfdc9f36f2e3fdb8f08c25
bcggxgcx <135641968+bcggxgcx@users.noreply.github.com>
2023-12-02 20:09:39 +0330

    Update config.py
    Signed-off-by: bcggxgcx <135641968+bcggxgcx@users.noreply.github.com>

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "13949344"))
 API_HASH = getenv("API_HASH", "408723003ad67fa8ab01ccc7f53e24ad")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6817393512:AAHksFyaUfon-fsR-zFMakciRK5rdlzd4wE")
+BOT_TOKEN = getenv("BOT_TOKEN", "6901906356:AAGQ6KrpNzn36MdkQU29Uu1Ed9S0TP-HsPQ")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "6539691387"))
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID", "6539691387"))
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("SESSION", "BAA4dkPOjfy7HFoxJQNA2hnk7sFXoZQ5lcgiULbpp1DThNT7Wk9mnzYXI2h3zB96VwWD1vXMApDBfng6h2tB088GxUkfvpX5g4q2fu6MzD8DEAn9Zx1AbAKfjyMz5fBiZ9kjOLFVEQOLimmlwvbDHXtidsOABw9Q5yZ7eQ9HyUTIzEOpi03v-EXkL-AanyHna9xF1ZPfHlZY8-IOJBTkx3OyXolmM61wsl4TBeZJ1r6AbaNEG2TLs803rOMhjTflvuiI6rmkvMM8_xd2v848YX6SYYZyArIpHYQAGk2w5VJP69dN1torBqrxpr_4H17hqG342e-nzuw_XXT1gWsj8EZUAAAAAYXLxXsA")
+SESSION = getenv("SESSION", "BAAazQmIudUYiXrpAqTMsTwJbQDJPGZlu4T-dfb3dbIAxR60BnGIN5ZmdvxSAtyssqPncCobQ8pniuHNbPhGoL_O0wO5jH4ewJYGEt3Wvf6rMv13Th1b3pgKsxjZ08kKNzfl0xkONvRKx_6tMMH7zPQ6VVI_rVT1kIyWC7uYA6dLFZvucRpLwXV-vQ7t1Z_BET37nybOfxDAMicRBD_mnUBrIFX1KwHkDRvUx7vPjFTTfAI7LDnotossLzQR6FMNAidj2j9cyOrNZjlmb2rkhH1NMvpMOZsQW5gVD2cMcaigIKymYK4N92KoQZKJosB2onZIuP6rlqs-FK2pTC72fRAeAAAAAYXLxXsA")
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
